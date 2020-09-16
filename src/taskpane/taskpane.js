a5cac7ece9c6e06b75b171fcf327a169bda2a63c
Powrie <ipowrie@hoopp.com>
2020-09-16 12:29:52 -0400

    card icons for attachments

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -27,4 +27,23 @@ export async function run() {
 
   // Write message property value to the task pane
   document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
+
+  // Generate attachment list
+  var outputString = "";
+
+  if (item.attachments.length > 0) {
+      for (i = 0 ; i < item.attachments.length ; i++) {
+          var attachment = item.attachments[i];
+
+          outputString += '<div class="container"><label>';
+          outputString += '<input type="radio" name="attachments" id="attachment' + i + '" class="card-input-element" value="' + attachment.id + '"/>';
+          outputString += '<div class="panel panel-default card-input form-check card"><img src="../../assets/icon-32.png" class="col-2" alt=""/><div class="col-10">';
+          outputString += '<h5 class="card-title">' + attachment.name + '</h5>';
+          outputString += '<h6 class="card-subtitle mb-2 text-muted">' + attachement.contentType + ' ' + attachment.size + '</h6>';
+          outputString += '<p class="card-text">Could add some additional descriptive text somehow........ from parser?</p>';
+          outputString += '</div></label></div></div>';
+      }
+  }
+
+  document.getElementById("msg-attachments").innerHTML = outputString;
 }
